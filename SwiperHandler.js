1cfe2ebd10a912b60209603536984a37b57b48ed
Viktor Maár <viktor.maar@exktasoftware.se>
2018-11-01 08:05:56 +0100

    Wait for loading before use Swiper

--- SwiperHandler.js
+++ SwiperHandler.js
@@ -1,166 +1,168 @@
 (function () {
     'use strict';
-    var Swiper = require('swiper');
-    if (typeof Swiper.default != 'undefined') {
-        Swiper = Swiper.default;
-    }
+    require(['swiper'], function(Swiper) {
 
-    /*===========================
-    SwiperHandler
-    ===========================*/
-    var SwiperHandler = {
-        window: null,
-        swiperId: 1,
-        swipers: {},
-        initialized: false,
-        initialize: function() {
-            var self = this;
-            $('.initSwiper').each(function() {
-                var $this = $(this);
-                if ($this.hasClass('initialized')) {
-                    return true;
-                }
-                var idAttribute = 'swiperId-'+ self.swiperId;
-                var selector = '#'+ idAttribute;
+        if (typeof Swiper.default != 'undefined') {
+            Swiper = Swiper.default;
+        }
 
-                $this.attr('id', idAttribute);
-                SwiperHandler.swipers[self.swiperId] = {
-                    selector: selector,
-                    swiperSelector: selector +' .swiper-container',
-                    element: $this,
-                    settings: eval('(' + $this.attr('data-swiper-settings') + ')'),
-                    instance: null,
-                    currentBreakpoint: 0,
-                    amountOfSlides: $('.swiper-slide', $this).length,
-                };
-                self.swiperId++;
-            });
+        /*===========================
+        SwiperHandler
+        ===========================*/
+        var SwiperHandler = {
+            window: null,
+            swiperId: 1,
+            swipers: {},
+            initialized: false,
+            initialize: function() {
+                var self = this;
+                $('.initSwiper').each(function() {
+                    var $this = $(this);
+                    if ($this.hasClass('initialized')) {
+                        return true;
+                    }
+                    var idAttribute = 'swiperId-'+ self.swiperId;
+                    var selector = '#'+ idAttribute;
 
-            if (SwiperHandler.initialized === false) {
-                SwiperHandler.window = $(window);
-                SwiperHandler.window.resize(function() {
-                    clearTimeout(SwiperHandler.window.resizedFinished);
-                    SwiperHandler.window.resizedFinished = setTimeout(SwiperHandler.rebuildOnResize, 250);
+                    $this.attr('id', idAttribute);
+                    SwiperHandler.swipers[self.swiperId] = {
+                        selector: selector,
+                        swiperSelector: selector +' .swiper-container',
+                        element: $this,
+                        settings: eval('(' + $this.attr('data-swiper-settings') + ')'),
+                        instance: null,
+                        currentBreakpoint: 0,
+                        amountOfSlides: $('.swiper-slide', $this).length,
+                    };
+                    self.swiperId++;
                 });
-            }
 
-            SwiperHandler.buildAll(true);
-
-            SwiperHandler.initialized = true;
-        },
-        rebuildOnResize: function() {
-            SwiperHandler._each(function(swiperId, swiper) {
-                var swiper = SwiperHandler.swipers[swiperId];
-                var breakpoint = SwiperHandler.getBreakpoint(swiperId);
-
-                if (swiper.currentBreakpoint != breakpoint) {
-                    SwiperHandler.build(swiperId);
-                }
-            });
-
-            SwiperHandler.syncSwipers();
-        },
-        buildAll: function(dontDestroy) {
-            SwiperHandler._each(function(swiperId, swiper) {
-                SwiperHandler.build(swiperId, dontDestroy);
-            });
-
-            SwiperHandler.syncSwipers();
-        },
-        build: function(id, dontDestroy) {
-            var dontDestroy = dontDestroy || false;
-            var swiper = SwiperHandler.swipers[id];
-
-            if (dontDestroy === false && swiper.instance != null) {
-                SwiperHandler.destroy(id);
-            }
-
-            swiper.instance = new Swiper(swiper.swiperSelector, SwiperHandler.getSettings(id));
-            SwiperHandler.pauseOnHover(id);
-            swiper.element.addClass('initialized');
-        },
-        syncSwipers: function() {
-            SwiperHandler._each(function(swiperId, swiper) {
-                var syncGroup = swiper.settings.syncGroup;
-                if (syncGroup) {
-                    SwiperHandler._each(function(iterationSwiperId, iterationSwiper) {
-                        if (swiper.instance.controller && swiperId != iterationSwiperId && iterationSwiper.settings.syncGroup == syncGroup) {
-                            swiper.instance.controller.control = iterationSwiper.instance;
-                        }
+                if (SwiperHandler.initialized === false) {
+                    SwiperHandler.window = $(window);
+                    SwiperHandler.window.resize(function() {
+                        clearTimeout(SwiperHandler.window.resizedFinished);
+                        SwiperHandler.window.resizedFinished = setTimeout(SwiperHandler.rebuildOnResize, 250);
                     });
                 }
-            });
-        },
-        pauseOnHover: function(id) {
-            var swiper = SwiperHandler.swipers[id];
-            if ( ! swiper.settings.pauseOnHover || (swiper.instance.autoplay && swiper.instance.autoplay.running == false) ) {
-                return;
-            }
 
-            swiper.element.on('mouseover', function() {
-                swiper.instance.autoplay.stop();
-            });
-            swiper.element.on('mouseleave', function() {
-                swiper.instance.autoplay.start();
-            })
-        },
-        getSettings: function(id) {
-            var swiper = SwiperHandler.swipers[id];
-            var settings = swiper.settings || {};
-            var newSettings = settings.default || {};
-            var breakpoint = SwiperHandler.getBreakpoint(id);
-            var breakpointSettings = {};
+                SwiperHandler.buildAll(true);
 
-            swiper.currentBreakpoint = breakpoint;
-            if (breakpoint > 0) {
-                breakpointSettings = swiper.settings.breakpoints[breakpoint];
-            }
-            newSettings = $.extend({}, newSettings, breakpointSettings);
+                SwiperHandler.initialized = true;
+            },
+            rebuildOnResize: function() {
+                SwiperHandler._each(function(swiperId, swiper) {
+                    var swiper = SwiperHandler.swipers[swiperId];
+                    var breakpoint = SwiperHandler.getBreakpoint(swiperId);
 
-            if (settings.centerSlidesIfTooFew && swiper.amountOfSlides < newSettings.slidesPerView) {
-                newSettings.centeredSlides = true;
-            }
+                    if (swiper.currentBreakpoint != breakpoint) {
+                        SwiperHandler.build(swiperId);
+                    }
+                });
 
-            //Add empty navigation if not exists
-            newSettings.navigation = newSettings.navigation || {};
-            //Add current swiper-selector before prev/next nav button
-            newSettings.navigation.prevEl = swiper.selector + ' ' + (newSettings.navigation.prevEl || '.swiper-button-prev');
-            newSettings.navigation.nextEl = swiper.selector + ' ' + (newSettings.navigation.nextEl || '.swiper-button-next');
+                SwiperHandler.syncSwipers();
+            },
+            buildAll: function(dontDestroy) {
+                SwiperHandler._each(function(swiperId, swiper) {
+                    SwiperHandler.build(swiperId, dontDestroy);
+                });
 
-            return newSettings;
-        },
-        getBreakpoint: function(id) {
-            var swiper = SwiperHandler.swipers[id];
+                SwiperHandler.syncSwipers();
+            },
+            build: function(id, dontDestroy) {
+                var dontDestroy = dontDestroy || false;
+                var swiper = SwiperHandler.swipers[id];
 
-            for (var width in swiper.settings.breakpoints) {
-              if (SwiperHandler.window.width() <= width) {
-                return width;
-              }
-            }
+                if (dontDestroy === false && swiper.instance != null) {
+                    SwiperHandler.destroy(id);
+                }
 
-            return 0;
-        },
-        destroyAll: function() {
-            SwiperHandler._each(function(swiperId, swiper) {
-                SwiperHandler.destroy(swiperId);
-            });
-        },
-        destroy: function(id) {
-            if (SwiperHandler.swipers[id].instance != null) {
-                SwiperHandler.swipers[id].instance.destroy(false, true);
-            }
-        },
-        _each: function(callback) {
-            if (typeof callback != 'function') {
-                return false;
-            }
-            $.each(SwiperHandler.swipers, function(swiperId, swiper) {
-                callback(swiperId, swiper);
-            });
-        },
-    };
-    
-    window.SwiperHandler = SwiperHandler;
+                swiper.instance = new Swiper(swiper.swiperSelector, SwiperHandler.getSettings(id));
+                SwiperHandler.pauseOnHover(id);
+                swiper.element.addClass('initialized');
+            },
+            syncSwipers: function() {
+                SwiperHandler._each(function(swiperId, swiper) {
+                    var syncGroup = swiper.settings.syncGroup;
+                    if (syncGroup) {
+                        SwiperHandler._each(function(iterationSwiperId, iterationSwiper) {
+                            if (swiper.instance.controller && swiperId != iterationSwiperId && iterationSwiper.settings.syncGroup == syncGroup) {
+                                swiper.instance.controller.control = iterationSwiper.instance;
+                            }
+                        });
+                    }
+                });
+            },
+            pauseOnHover: function(id) {
+                var swiper = SwiperHandler.swipers[id];
+                if ( ! swiper.settings.pauseOnHover || (swiper.instance.autoplay && swiper.instance.autoplay.running == false) ) {
+                    return;
+                }
+
+                swiper.element.on('mouseover', function() {
+                    swiper.instance.autoplay.stop();
+                });
+                swiper.element.on('mouseleave', function() {
+                    swiper.instance.autoplay.start();
+                })
+            },
+            getSettings: function(id) {
+                var swiper = SwiperHandler.swipers[id];
+                var settings = swiper.settings || {};
+                var newSettings = settings.default || {};
+                var breakpoint = SwiperHandler.getBreakpoint(id);
+                var breakpointSettings = {};
+
+                swiper.currentBreakpoint = breakpoint;
+                if (breakpoint > 0) {
+                    breakpointSettings = swiper.settings.breakpoints[breakpoint];
+                }
+                newSettings = $.extend({}, newSettings, breakpointSettings);
+
+                if (settings.centerSlidesIfTooFew && swiper.amountOfSlides < newSettings.slidesPerView) {
+                    newSettings.centeredSlides = true;
+                }
+
+                //Add empty navigation if not exists
+                newSettings.navigation = newSettings.navigation || {};
+                //Add current swiper-selector before prev/next nav button
+                newSettings.navigation.prevEl = swiper.selector + ' ' + (newSettings.navigation.prevEl || '.swiper-button-prev');
+                newSettings.navigation.nextEl = swiper.selector + ' ' + (newSettings.navigation.nextEl || '.swiper-button-next');
+
+                return newSettings;
+            },
+            getBreakpoint: function(id) {
+                var swiper = SwiperHandler.swipers[id];
+
+                for (var width in swiper.settings.breakpoints) {
+                  if (SwiperHandler.window.width() <= width) {
+                    return width;
+                  }
+                }
+
+                return 0;
+            },
+            destroyAll: function() {
+                SwiperHandler._each(function(swiperId, swiper) {
+                    SwiperHandler.destroy(swiperId);
+                });
+            },
+            destroy: function(id) {
+                if (SwiperHandler.swipers[id].instance != null) {
+                    SwiperHandler.swipers[id].instance.destroy(false, true);
+                }
+            },
+            _each: function(callback) {
+                if (typeof callback != 'function') {
+                    return false;
+                }
+                $.each(SwiperHandler.swipers, function(swiperId, swiper) {
+                    callback(swiperId, swiper);
+                });
+            },
+        };
+
+        window.SwiperHandler = SwiperHandler;
+    });
 })();
 
 /*===========================
